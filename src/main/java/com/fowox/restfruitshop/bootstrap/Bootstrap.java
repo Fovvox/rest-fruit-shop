eb800d27967ab50ea68aa3115ebb347212ba1a0d
Yurii Kondratenko <yurii.condratenco@gmail.com>
2022-06-05 18:11:38 +0200

    added bootstrap for customer

--- src/main/java/com/fowox/restfruitshop/bootstrap/Bootstrap.java
+++ src/main/java/com/fowox/restfruitshop/bootstrap/Bootstrap.java
@@ -1,7 +1,9 @@
 package com.fowox.restfruitshop.bootstrap;
 
 import com.fowox.restfruitshop.domain.Category;
+import com.fowox.restfruitshop.domain.Customer;
 import com.fowox.restfruitshop.repository.CategoryRepository;
+import com.fowox.restfruitshop.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +11,49 @@ import org.springframework.stereotype.Component;
 public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
+    private final CustomerRepository customerRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
         this.categoryRepository = categoryRepository;
+        this.customerRepository = customerRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        categoryBootstrap();
+        customerBootstrap();
+    }
+
+    private void customerBootstrap() {
+        Customer joe = new Customer();
+        joe.setFirstName("Joe");
+        joe.setLastName("Newman");
+
+        Customer michael = new Customer();
+        michael.setFirstName("Michael");
+        michael.setLastName("Lachappele");
+
+        Customer david = new Customer();
+        david.setFirstName("David");
+        david.setLastName("Winter");
+
+        Customer anne = new Customer();
+        anne.setFirstName("Anne");
+        anne.setLastName("Hine");
+
+        Customer alice = new Customer();
+        alice.setFirstName("Alice");
+        alice.setLastName("Eastman");
+
+        customerRepository.save(joe);
+        customerRepository.save(michael);
+        customerRepository.save(david);
+        customerRepository.save(anne);
+        customerRepository.save(alice);
+
+    }
+
+    private void categoryBootstrap() {
         Category fruits = new Category();
         fruits.setName("Fruits");
 
