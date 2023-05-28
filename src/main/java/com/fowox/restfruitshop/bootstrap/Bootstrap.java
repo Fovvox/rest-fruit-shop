9fea132a81f3d6b70055c89d97fd567de95ff43a
Yurii Kondratenko <yurii.condratenco@gmail.com>
2023-05-28 20:35:55 +0200

    added vendors Bootstrap

--- src/main/java/com/fowox/restfruitshop/bootstrap/Bootstrap.java
+++ src/main/java/com/fowox/restfruitshop/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package com.fowox.restfruitshop.bootstrap;
 
 import com.fowox.restfruitshop.domain.Category;
 import com.fowox.restfruitshop.domain.Customer;
+import com.fowox.restfruitshop.domain.Vendor;
 import com.fowox.restfruitshop.repository.CategoryRepository;
 import com.fowox.restfruitshop.repository.CustomerRepository;
+import com.fowox.restfruitshop.repository.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,19 @@ public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
     private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         categoryBootstrap();
         customerBootstrap();
+        vendorBootstrap();
     }
 
     private void customerBootstrap() {
@@ -74,5 +79,16 @@ public class Bootstrap implements CommandLineRunner {
         categoryRepository.save(fresh);
         categoryRepository.save(exotic);
         categoryRepository.save(nuts);
+    }
+
+    private void vendorBootstrap() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Vendor 1");
+        vendorRepository.save(vendor1);
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Vendor 2");
+        vendorRepository.save(vendor2);
+
     }
 }
