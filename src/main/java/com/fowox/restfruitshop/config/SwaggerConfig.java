f360ed08b54621c69156de2da5044bdd2b38e2df
Yurii Kondratenko <yurii.condratenco@gmail.com>
2023-06-03 15:57:52 +0200

    added metadata to Swagger documentation

--- src/main/java/com/fowox/restfruitshop/config/SwaggerConfig.java
+++ src/main/java/com/fowox/restfruitshop/config/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
 
 @EnableSwagger2
 @Configuration
@@ -19,8 +23,24 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
 
+    }
+
+    private ApiInfo metaData(){
+        Contact contact = new Contact("Yurii Kondratenko", "https://github.com/Fovvox", "yurii.condratenco@gmail.com");
+
+        return new ApiInfo(
+                "Spring Rest Fruit Shop",
+                "",
+                "1.0",
+                "Terms of Service: blah blah blah",
+                contact,
+                "Apache License, Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
     }
 
 }
